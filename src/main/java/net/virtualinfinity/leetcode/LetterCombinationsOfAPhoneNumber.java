321543bbd44d73756736bcb0febfd3e3d292dc0a
Daniel Pitts <git@stochastictinker.com>
2018-10-27 11:18:35 -0700

    Create a faster solution to LetterCombinationsOfAPhoneNumber.

--- src/main/java/net/virtualinfinity/leetcode/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/net/virtualinfinity/leetcode/LetterCombinationsOfAPhoneNumber.java
@@ -1,36 +1,53 @@
 package net.virtualinfinity.leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
  */
 public class LetterCombinationsOfAPhoneNumber {
-    private static final String[][] possibilities = {
-            {"a", "b", "c"}, // 2
-            {"d", "e", "f"}, // 3
-            {"g", "h", "i"}, // 4
-            {"j", "k", "l"}, // 5
-            {"m", "n", "o"}, // 6
-            {"p", "q", "r", "s"}, // 7
-            {"t", "u", "v"}, // 8
-            {"w", "x", "y", "z"}, // 9
-    };
+    private static final List<char[]> possibilities = Arrays.asList(new char[][]{
+            {'a', 'b', 'c'}, // 2
+            {'d', 'e', 'f'}, // 3
+            {'g', 'h', 'i'}, // 4
+            {'j', 'k', 'l'}, // 5
+            {'m', 'n', 'o'}, // 6
+            {'p', 'q', 'r', 's'}, // 7
+            {'t', 'u', 'v'}, // 8
+            {'w', 'x', 'y', 'z'}, // 9
+    });
 
     public List<String> letterCombinations(String digits) {
-        return digits.chars()
-                .mapToObj(ch -> possibilities[ch - '2'])
-                .map(Arrays::asList)
-                .reduce(
-                        (left, right) ->
-                                left.stream()
-                                        .flatMap(
-                                                leftString -> right.stream()
-                                                        .map(rightString -> leftString + rightString)
-                                        ).collect(Collectors.toList())
-                ).orElseGet(Collections::emptyList);
+        if (digits.equals("")) {
+            return Collections.emptyList();
+        }
+        int[] idx = new int[digits.length()];
+        char[][] combos = new char[digits.length()][];
+        char[] builder = new char[digits.length()];
+        for (int i = 0; i < combos.length; ++i) {
+            combos[i] = possibilities.get(digits.charAt(i)-'2');
+            builder[i] = combos[i][0];
+        }
+        List<String> results = new ArrayList<>();
+        do {
+            results.add(String.valueOf(builder));
+            int i = 0;
+            do {
+                idx[i]++;
+                if (idx[i] < combos[i].length) {
+                    break;
+                }
+                idx[i] = 0;
+                builder[i] = combos[i][0];
+                i++;
+                if (i >= idx.length) {
+                    return results;
+                }
+            } while (true);
+            builder[i] = combos[i][idx[i]];
+        } while (true);
     }
 }
